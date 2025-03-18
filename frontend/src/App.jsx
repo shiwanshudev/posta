74b50a42ad8e79e0f7367b464c4576556cdb30f0
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2025-03-18 18:01:45 +0530

    Add tailwind and setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Login from "../components/Login";
+import Nav from "../components/Nav";
+
+function App() {
+  return (
+    <main>
+      <Nav />
+      <Login />
+    </main>
+  );
+}
+
+export default App;
